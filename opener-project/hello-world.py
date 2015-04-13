a789fa3b76ec8d8e3451489403fa1727c679dbb2
Fang Yi Lin <lfy1030@gmail.com>
2015-04-12 17:44:10 -0700

    Opener Project Part 1
    Part 1 completed!

---
+++ opener-project/hello-world.py
@@ -0,0 +1,7 @@
+print("Hello world")
+myname = "Fang"
+print("My name is", myname)
+myage=21
+print("I am", myage, "years old")
+print("That is roughly", myage*365, "days")
+print("Or,", myage*365*24*60*60, "seconds")
